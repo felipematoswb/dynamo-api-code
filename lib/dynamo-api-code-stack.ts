43b9245c808f8166ac54b19f1023b2340c985339
Felipe Matos <felipematoswb@gmail.com>
2021-12-07 20:11:32 -0300

    start

--- lib/dynamo-api-code-stack.ts
+++ lib/dynamo-api-code-stack.ts
@@ -1,15 +1,22 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
 
 export class DynamoApiCodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  const table = new dynamodb.Table(this, 'Table', {
+    partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+    billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    tableName: 'books'
+  });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DynamoApiCodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  const fn = new lambda.Function(this, 'MyFunction', {
+    runtime: lambda.Runtime.NODEJS_12_X,
+    handler: 'index.handler',
+    code: lambda.Code.fromAsset('lambda'),
+  });
+
   }
 }
