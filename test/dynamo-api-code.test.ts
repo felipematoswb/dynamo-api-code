0906a28f50a05cdf5d482e70aa94947da7b0f4f4
Felipe Matos <felipematoswb@gmail.com>
2021-12-07 17:49:02 -0300

    Initial commit

---
+++ test/dynamo-api-code.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as DynamoApiCode from '../lib/dynamo-api-code-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamo-api-code-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamoApiCode.DynamoApiCodeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
